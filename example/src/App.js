044d0523adc8f7a80137efe9c24d6465011a9c3a
Jimmy Höglund <jimmy@saits.se>
2019-07-10 17:17:16 +0200

    Context values name change (#4)
    * Change some DrawableContextValue key names.
    - Export useDrawableContext hook for easier access to the context.
    - Update README.md
    - Update example
    - Update PropTypes and default props
    
    * Update version in package-json

--- example/src/App.js
+++ example/src/App.js
@@ -2,21 +2,19 @@ import React from "react"
 import DrawableOverlay from "react-drawable-overlay"
 import Toolbar from "./components/Toolbar"
 
-function App() {
-  const renderDrawableContent = () => (
-    <div style={{ height: 500, width: 1000 }} className="drawable-content" />
-  )
+const renderDrawableContent = () => (
+  <div style={{ height: 500, width: 1000 }} className="drawable-content" />
+)
 
-  return (
-    <div className="wrapper">
-      <DrawableOverlay
-        renderDrawableContent={renderDrawableContent}
-        defaultBrushColor="#0000FF"
-      >
-        <Toolbar />
-      </DrawableOverlay>
-    </div>
-  )
-}
+const App = () => (
+  <div className="wrapper">
+    <DrawableOverlay
+      renderDrawableContent={renderDrawableContent}
+      defaultBrushColor="#0000FF"
+    >
+      <Toolbar />
+    </DrawableOverlay>
+  </div>
+)
 
 export default App
