81a905039f10bc62814a3271bf4094a9e02b99d5
Jimmy <jimhog2@outlook.com>
2019-07-11 14:38:01 +0200

    Fix typedefs

--- types.d.ts
+++ types.d.ts
@@ -1,42 +1,37 @@
-import React from "react"
+import * as React from "react"
 
-declare module ReactDrawableOverlay {
-  declare const DrawableOverlay: React.ComponentType<DrawableOverlayProps>
-
-  export function useDrawableContext(): DrawableContextValues
-  export const DrawableContext: React.Context<DrawableContextValues>
-
-  export interface DrawableOverlayProps {
-    widthOffset?: number
-    heightOffset?: number
-    defaultBrushColor?: string
-    inDrawMode?: boolean
-    className?: string
-    renderDrawableContent: () => JSX.Element
-    onAddToHistory?: (drawing: string) => void
-  }
-
-  export interface DrawableContextValues {
-    undo: () => void
-    redo: () => void
-    reset: () => void
-    setInitialDrawing: (drawing: string) => void
-    currentHistoryIndex: number
-    history: string[]
-    drawMode: DrawModeTypes
-    setDrawMode: (drawMode: DrawModeTypes) => void
-    brushSize: number
-    setBrushSize: (size: number) => void
-    eraserSize: number
-    setEraserSize: (size: number) => void
-    brushColor: string
-    setBrushColor: (hexadecimal: string) => void
-  }
-  export type DrawModeTypes = "brush" | "eraser"
-  export enum DrawMode {
-    Brush = "brush",
-    Eraser = "eraser",
-  }
+export interface DrawableContextValues {
+  undo: () => void
+  redo: () => void
+  reset: () => void
+  setInitialDrawing: (drawing: string) => void
+  currentHistoryIndex: number
+  history: string[]
+  drawMode: DrawModeTypes
+  setDrawMode: (drawMode: DrawModeTypes) => void
+  brushSize: number
+  setBrushSize: (size: number) => void
+  eraserSize: number
+  setEraserSize: (size: number) => void
+  brushColor: string
+  setBrushColor: (hexadecimal: string) => void
 }
-
-export default ReactDrawableOverlay
+export const DrawableContext: React.Context<DrawableContextValues>
+export interface DrawableOverlayProps {
+  widthOffset?: number
+  heightOffset?: number
+  defaultBrushColor?: string
+  inDrawMode?: boolean
+  children?: JSX.Element | JSX.Element[]
+  className?: string
+  renderDrawableContent: () => JSX.Element
+  onAddToHistory?: (drawing: string) => void
+}
+export type DrawModeTypes = "brush" | "eraser"
+export enum DrawMode {
+  Brush = "brush",
+  Eraser = "eraser",
+}
+export declare function useDrawableContext(): DrawableContextValues
+declare class DrawableOverlay extends React.Component<DrawableOverlayProps> {}
+export default DrawableOverlay
